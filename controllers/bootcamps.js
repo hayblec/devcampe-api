ea7051d1370647f08210b2896dd4b842525c64bf
Charles Hayble <hayblec@gmail.com>
2023-04-29 21:13:33 +0100

    Added controller methods

---
+++ controllers/bootcamps.js
@@ -0,0 +1,40 @@
+// @desc    GET all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+};
+
+// @desc    GET bootcamp
+// @route   GET /api/v1/bootcamps/:id
+// @access  Public
+exports.getBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show a bootcamps ${req.params.id}` });
+};
+
+// @desc    Create bootcamp
+// @route   POST /api/v1/bootcamps
+// @access  Private
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `Create new bootcamps` });
+};
+
+// @desc    Update bootcamp
+// @route   PUT /api/v1/bootcamps/:id
+// @access  Private
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamps ${req.params.id}` });
+};
+
+// @desc    Delete bootcamp
+// @route   DELETE /api/v1/bootcamps/:id
+// @access  Private
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamps ${req.params.id}` });
+};
